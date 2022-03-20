4273e5c279c7adb1927fdad706cba6c4b0a26aa6
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2022-03-20 21:56:50 +0700

    Initial Commit

---
+++ src/daftarmahasiswa/Mahasiswa.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daftarmahasiswa;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Mahasiswa {
+    private String nim;
+    private String nama;
+    private String nilai;
+    
+    public Mahasiswa(String n1, String n2, String n3){
+        nim = n1;
+        nama = n2;
+        nilai = n3;
+    }
+    
+    public void setNim(String n) {
+        nim = n;
+    }
+    
+    public void setNama(String n) {
+        nama = n;
+    }
+    
+    public void setNilai(String n) {
+        nilai = n;
+    }
+    
+    public String getNim() {
+        return nim;
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+    
+    public String getNilai() {
+        return nilai;
+    }
+}
